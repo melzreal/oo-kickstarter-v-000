4aa2826bcc414fe134348f959f27ca756b681842
Melissa Real <melissa.real@hotmail.com>
2018-08-12 14:39:26 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,7 @@ class Backer
   attr_accessor :backed_projects, :name
 
   def initialize(name)
-    @name = name 
+    @name = name
     @backed_projects = []
   end
 
--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,5 @@
 class Project
-  attr_accessor :backer
+  attr_accessor :backer, :title 
 
   def initialize
     @backers = []
